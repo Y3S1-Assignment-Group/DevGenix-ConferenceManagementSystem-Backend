4b2dcf35ff9e571b08d5c7002d7dcd5704b840a9
SenuraJayadeva <senurajayadeva@gmail.com>
2021-05-14 12:47:33 +0530

    basic project structure initialized

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//Using Cors
+app.use(cors());
+
+//Init Middleware( include  bodyparser through express)
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("ICAF Backend Api Running"));
+
+//Define Routes
+
+//-------------------Senura---------------------
+//app.use("/api/instructor/meal", require("./routes/api/meal"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
