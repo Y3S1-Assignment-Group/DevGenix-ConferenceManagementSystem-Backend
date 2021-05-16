5c19a3c4903dc03ddb5e7431858b91f4125552e8
ayodyabanuka <ayodyabanukafernando@gmail.com>
2021-05-16 16:15:32 +0530

    unapproved research papers

--- routes/researchpapers.route.js
+++ routes/researchpapers.route.js
@@ -1,14 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authReviewer = require("../middleware/authReviewer");
+const authReviewer = require('../middleware/authReviewer');
 
 const {
   getApprovedResearchPapers,
-} = require("../controllers/researchpapers.controller");
+  getUnapprovedResearchPapers,
+} = require('../controllers/researchpapers.controller');
 
 //@route  GET api/ResearchPapers
 //@desc   Get Approved Research Papers
 //@access public
-router.get("/", getApprovedResearchPapers);
+router.get('/approved', getApprovedResearchPapers);
+
+//@route  GET api/ResearchPapers
+//@desc   Get Unapproved Research Papers
+//@access public
+router.get('/unapproved', getUnapprovedResearchPapers);
 
 module.exports = router;
