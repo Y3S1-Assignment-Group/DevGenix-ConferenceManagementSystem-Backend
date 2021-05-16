f9488f74515aefa77ff0f31a8bffd02f064f1420
SenuraJayadeva <senurajayadeva@gmail.com>
2021-05-16 15:47:37 +0530

    Get Approved Research Papers

---
+++ controllers/researchpapers.controller.js
@@ -0,0 +1,17 @@
+const Researcher = require("../models/Researcher.model");
+
+//get Approved ResearchPapers
+const getApprovedResearchPapers = async (req, res) => {
+  try {
+    const researchPapers = await Researcher.find({
+      "researchPaper.approved": true,
+    }).select(
+      "firstName lastName jobStatus universityOrWorkPlace researchPaper"
+    );
+    res.json(researchPapers);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = { getApprovedResearchPapers };
