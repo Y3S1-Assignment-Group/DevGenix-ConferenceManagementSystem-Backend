c981cbccc1abb7a15d8b05f3cab71516aa20afb5
CandyPanda-LS <it19132310@my.sliit.lk>
2021-05-16 11:21:45 +0530

    presente registarion

---
+++ models/Presenter.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  role: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  jobStatus: {
+    type: String,
+  },
+  universityOrWorkPlace: {
+    type: String,
+  },
+  workshopAttends: [
+    {
+      workshopName: { type: String },
+    },
+  ],
+  statementOfInterest: {
+    type: String,
+  },
+});
+
+module.exports = Presenter = mongoose.model("Presenter", UserSchema);
