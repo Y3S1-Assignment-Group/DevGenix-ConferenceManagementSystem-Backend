5888523bde098a1291017497a4c6933a5ffc267f
Dilmi101 <dnimasha971@gmail.com>
2021-05-16 12:18:47 +0530

    Editor registration

---
+++ routes/authEditor.route.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/authEditor");
+const {
+  getEditorDetails,
+  loginEditor,
+  registerEditor,
+} = require("../controllers/authEditor.controller");
+
+//@route  POST api/authEditor
+//@desc   Register editor
+//@access Public
+router.post("/register", registerEditor);
+
+//@route  POST api/authEditor
+//@desc   authenticate user and get token(login)
+//@access Public
+// router.post("/", loginEditor);
+
+// //@route  GET api/authEditor
+// //@desc   Get editor details using jwt
+// //@access Admin
+// //Route restricted with authetication (JWT Token)
+// router.get("/", auth, getEditorDetails);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.use("/api/authadmin", require("./routes/authadmin.route"));
 //-------------------Attendee---------------------
 app.use("/api/authattendee", require("./routes/authAttendee.route"));
 
+//-------------------Editor---------------------
+app.use("/api/autheditor", require("./routes/authEditor.route"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
