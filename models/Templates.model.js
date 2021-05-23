e8bd047d2fd3fef05b98bb9b35ecf77a85d44ca6
SenuraJayadeva <senurajayadeva@gmail.com>
2021-05-23 20:48:27 +0530

    update

--- models/Templates.model.js
+++ models/Templates.model.js
@@ -1,17 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TemplateSchema = new Schema({
-  paperTemplate: {
-    WordLink: { type: String },
-    UpdatedDate: { type: Date },
+  templateName: {
+    type: String,
   },
-
-  workshopTemplate: {
-    WordLink: { type: String },
-    pptxLink: { type: String },
-    UpdatedDate: { type: Date },
+  fileLink: {
+    type: String,
   },
 });
 
-module.exports = Presenter = mongoose.model('Templates', TemplateSchema);
+module.exports = Presenter = mongoose.model("Templates", TemplateSchema);
--- routes/templates.route.js
+++ routes/templates.route.js
@@ -1,23 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/authEditor');
+const auth = require("../middleware/authEditor");
 const {
   addTemplate,
   updateTemplate,
   getTemplate,
-} = require('../controllers/temlate.controller');
+  deleteTemplate,
+} = require("../controllers/temlate.controller");
 
 //@route  get api/Templates
 //@desc   get templates
 //@access Public
-router.get('/', getTemplate);
+router.get("/", getTemplate);
 //@route  POST api/Templates
 //@desc   Add Template
 //@access Public
-router.post('/', auth, addTemplate);
+router.post("/", auth, addTemplate);
 //@route  PUT api/Templates
 //@desc   Update Templates
 //@access Public
-router.put('/', auth, updateTemplate);
+router.put("/", auth, updateTemplate);
+//@route  PUT api/Templates
+//@desc   Update Templates
+//@access Public
+router.delete("/:id", auth, deleteTemplate);
 
 module.exports = router;
