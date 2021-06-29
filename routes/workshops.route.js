b83b9d2b082d1b51cb965138481a54477dc4b893
SenuraJayadeva <senurajayadeva@gmail.com>
2021-06-29 17:46:34 +0530

    add function to get presenter workshops by presenters id

--- routes/workshops.route.js
+++ routes/workshops.route.js
@@ -1,26 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/authReviewer');
+const auth = require("../middleware/authReviewer");
 
 const {
   getApprovedWorkshops,
   getUnapprovedWorkshops,
   approveWorkshops,
-} = require('../controllers/workshops.controller');
+  getWorkshopsByPresenter,
+} = require("../controllers/workshops.controller");
 
 //@route  GET api/Workshops
 //@desc   Get Approved workshops
 //@access public
-router.get('/approved', getApprovedWorkshops);
+router.get("/approved", getApprovedWorkshops);
 
 //@route  GET api/Workshops
 //@desc   Get Approved workshops
 //@access public
-router.get('/unapproved', auth, getUnapprovedWorkshops);
+router.get("/unapproved", auth, getUnapprovedWorkshops);
 
 //@route  POST api/Workshops/approveWorkshop
 //@desc   approve Workshops
 //@access public
 router.put("/approveWorkshop", auth, approveWorkshops);
+
+//@route  POST api/Workshops/approveWorkshop
+//@desc   approve Workshops
+//@access public
+router.get("/workshopbypresenter", auth, getWorkshopsByPresenter);
 
 module.exports = router;
